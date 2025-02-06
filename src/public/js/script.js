7fbc9d303f2e1f5fdd87f2fee7545fa644ecdcbf
ENSBWJSNWBANJAY <celogilak@gmail.com>
2025-02-06 08:30:30 +0700

    Update script.js

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -612,12 +612,6 @@ function startAnimation() {
     }, totalDuration);
 }
 
-// Preloader
-window.addEventListener("load", function () {
-  const preloader = document.getElementById("preloader");
-  preloader.style.display = "none";
-});
-
 // Back to Top Button
 document.addEventListener("DOMContentLoaded", function () {
   const backToTopButton = document.querySelector(".back-to-top");
